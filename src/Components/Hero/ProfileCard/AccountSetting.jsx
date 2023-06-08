0f6773d3ad8cb36c2e4d7bff87eeafa05bb49e1e
Abhishek <abhishekdixit9024@gmail.com>
2023-06-08 15:02:06 +0530

    added two rows in the form for account settings

--- src/Components/Hero/ProfileCard/AccountSetting.jsx
+++ src/Components/Hero/ProfileCard/AccountSetting.jsx
@@ -1,7 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import { Form, Button, Card } from "react-bootstrap";
 
 const AccountSetting = () => {
-  return <div>AccountSetting</div>;
+  const [formData, setFormData] = useState({
+    DOB: "1999-09-28",
+    DOJ: "2023-05-12",
+  });
+
+  return (
+    <div style={{ flexBasis: "80%", textAlign: "start" }}>
+      <Card className="hero-cards  ">
+        <Form className="mx-5 mt-2">
+          <Form.Group className="mb-3" controlId="formBasicDOB">
+            <Form.Label>Date of Birth</Form.Label>
+            <Form.Control
+              type="date"
+              value={formData.DOB}
+              onChange={(e) =>
+                setFormData({ ...formData, DOB: e.target.value })
+              }
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-3 " controlId="formBasicDOJ">
+            <Form.Label>Date of Joining</Form.Label>
+            <Form.Control
+              type="date"
+              value={formData.DOJ}
+              onChange={(e) =>
+                setFormData({ ...formData, DOJ: e.target.value })
+              }
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </Card>
+    </div>
+  );
 };
 
 export default AccountSetting;
