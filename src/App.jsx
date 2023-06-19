cc48bdba4c52d7be20c09b0aa0bbd54d27d0d281
Abhishek <abhishekdixit9024@gmail.com>
2023-06-19 13:19:30 +0530

    added the auto logout for the application

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import RouteComponents from "./Components/routes";
-import React from "react";
+import React, { useEffect } from "react";
 import { NavBar, SubNavBar, Banner } from "./Components/NavBar";
 import ProfileCard from "./Components/Hero/ProfileCard";
 import { CardGroup } from "react-bootstrap";
@@ -10,6 +10,27 @@ import { auth } from "./utils/firebase";
 
 function App() {
   const [user, loading] = useAuthState(auth);
+
+  //Auto Logout on window close
+  useEffect(() => {
+    const handleAppClose = () => {
+      auth
+        .signOut()
+        .then(() => {
+          // Logout successful
+        })
+        .catch((error) => {
+          // An error occurred while logging out
+          console.log(error);
+        });
+    };
+
+    window.addEventListener("beforeunload", handleAppClose);
+
+    return () => {
+      window.removeEventListener("beforeunload", handleAppClose);
+    };
+  }, []);
 
   return (
     <div className="App mx-auto">
