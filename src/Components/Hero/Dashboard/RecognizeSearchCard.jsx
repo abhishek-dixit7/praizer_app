a40619a9b82943f33f7cdb94715cfa13f992ef10
shoyab8382 <shoya8382alam@gmail.com>
2023-06-19 09:53:45 +0530

    SearchBox changed

--- src/Components/Hero/Dashboard/RecognizeSearchCard.jsx
+++ src/Components/Hero/Dashboard/RecognizeSearchCard.jsx
@@ -1,20 +1,8 @@
 import React, { useRef } from "react";
-import { Card, InputGroup, Form } from "react-bootstrap";
+import { Card, Form } from "react-bootstrap";
 
 const RecognizeSearchCard = () => {
   const search = useRef();
-
-  function SearchUser() {
-    console.log(search.current.value);
-    var spanElement = document.getElementById("seachinfo");
-    spanElement.classList.add("d-none");
-  }
-
-  function showSpan() {
-    var spanElement = document.getElementById("seachinfo");
-    spanElement.classList.toggle("d-none");
-  }
-
   return (
     <div className="mt-1 mx-1 text-start">
       <Card style={{ backgroundColor: "#B01C87", color: "white" }}>
@@ -23,26 +11,8 @@ const RecognizeSearchCard = () => {
           <Form.Control
             type="text"
             ref={search}
-            onChange={SearchUser}
-            onClick={showSpan}
+            placeholder="Search with Name or Email"
           />
-          <span
-            style={{
-              position: "absolute",
-              top: "3.45rem",
-              left: "1.5rem",
-              backgroundColor: "hsl(211, 3%, 96%)",
-              color: "black",
-              boxShadow: "1.5px 1.5px hsl(227, 7%, 28%)",
-              padding: "0.3rem",
-              zIndex: "1",
-              borderRadius: "0.2rem",
-            }}
-            id="seachinfo"
-            className="d-none"
-          >
-            Search by Name or Email.
-          </span>
         </Card.Body>
       </Card>
     </div>
